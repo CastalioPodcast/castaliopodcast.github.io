7bf5848c2f95cebbeffb4f076bb764e38fa51178
Elyézer Rezende <elyezermr@gmail.com>
2019-10-11 17:22:48 -0400

    Add global Top 5 page
    Show the Top 5 most recommended suggestions for each Top 5 category.

--- plugins/castalio/castalio.py
+++ plugins/castalio/castalio.py
@@ -1,4 +1,5 @@
 import json
+import operator
 import os
 
 import pelican
@@ -6,7 +7,7 @@ from docutils import nodes
 from docutils.parsers.rst import directives, roles, Directive
 
 
-TOP5_DATA = {}
+TOP5_STATS = {}
 
 
 class YouTubeEmbed(Directive):
@@ -64,24 +65,76 @@ class Top5(Directive):
         bullet_list = nodes.bullet_list()
         for field in node[0]:
             category = field[0].astext()
+            if not category in TOP5_STATS:
+                TOP5_STATS[category] = {}
             for item in [item.astext() for item in field[1][0]]:
                 top5_item = data.get(category, {}).get(item)
-                if top5_item:
-                    bullet_list.children.append(nodes.raw(
-                        '',
-                        self._li_template.format(
-                            category=self._category_text[category],
-                            name=item,
-                            url=top5_item['url'],
-                        ),
-                        format='html',
-                    ))
+                if not top5_item:
+                    return [self.state_machine.reporter.error(
+                        f'The Top 5 "{item}" was not found.',
+                        nodes.literal_block(self.block_text, self.block_text),
+                        line=self.lineno
+                    )]
+                bullet_list.children.append(nodes.raw(
+                    '',
+                    self._li_template.format(
+                        category=self._category_text[category],
+                        name=item,
+                        url=top5_item['url'],
+                    ),
+                    format='html',
+                ))
+                if item not in TOP5_STATS[category]:
+                    TOP5_STATS[category][item] = {
+                        'count': 0,
+                        'name': item,
+                        'url': top5_item['url'],
+                    }
+                TOP5_STATS[category][item]['count'] += 1
         return [
             nodes.title(self._header_title, '', nodes.Text(self._header_title)),
             bullet_list,
         ]
 
 
+class Top5Best(Directive):
+    _li_template = """<li>
+    <a class="reference external" href="{url}">{name}</a> ({count})
+    </li>
+    """
+    _category_text = {
+        'book': 'Top 5 Livros',
+        'movie': 'Top 5 Filmes',
+        'music': 'Top 5 Músicas',
+    }
+
+    def run(self):
+        output_nodes = []
+        for category in TOP5_STATS:
+            category_title = self._category_text[category]
+            output_nodes.append(
+                nodes.raw('', f'<h2>{category_title}</h2>', format='html')
+            )
+            items = sorted(TOP5_STATS[category].values(), key=operator.itemgetter('count'))[:5]
+            bullet_list = nodes.enumerated_list()
+            for item in items:
+                bullet_list.children.append(nodes.raw(
+                    '',
+                    self._li_template.format(**item),
+                    format='html',
+                ))
+            output_nodes.append(bullet_list)
+        return output_nodes
+
+
+def finalize(pelican):
+    # Clear the TOP5_STATS when pelican is done. This is useful when running
+    # the dev server so it resets the counts
+    TOP5_STATS.clear()
+
+
 def register():
-    directives.register_directive('youtube', YouTubeEmbed)
     directives.register_directive('top5', Top5)
+    directives.register_directive('top5best', Top5Best)
+    directives.register_directive('youtube', YouTubeEmbed)
+    pelican.signals.finalized.connect(finalize)
