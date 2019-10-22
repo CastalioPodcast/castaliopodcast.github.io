5951dcdbd3970e97033cafef03f6aeb2b8ec9f7d
Elyézer Rezende <elyezermr@gmail.com>
2019-10-22 14:24:56 -0400

    Update stats script
    * Use f-strings for string formatting
    * Ensure Flake8 is happy

--- .scripts/stats.py
+++ .scripts/stats.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
-# encoding: utf-8
 """Script to collect podcast stats.
 
 Requires:
 
-* Python 3.5+
+* Python 3.6+
 * aiohttp
 """
 import asyncio
 import codecs
-import json
 import os
 import re
 import sys
@@ -20,55 +18,60 @@ import async_timeout
 
 def get_name_and_url(fname):
     if not os.path.exists(fname):
-        print('File not found: {}'.format(fname))
+        print(f'File not found: {fname}')
         return (None, None)
 
     if os.path.isdir(fname):
-        print("Ignoring folder {} and it's contents".format(fname))
+        print(f"Ignoring folder {fname} and it's contents")
         return (None, None)
 
     if not fname.endswith('.rst'):
-        print('Not an rst file: {}'.format(fname))
+        print(f'Not an rst file: {fname}')
         return (None, None)
 
     with codecs.open(fname, 'r', 'utf-8') as fd:
         for line in fd:
-            match =  re.match("^\.\. podcast:: (.*)$", line)
+            match = re.match(r'^\.\. podcast:: (.*)$', line)
             if match:
                 break
         else:
-            print("File '{}' isn't about an episode!".format(fname))
+            print(f"File '{fname}' isn't about an episode!")
             return (None, None)
 
     archive_episode_name = match.groups()[0]
-    url = "https://archive.org/details/{}&output=json".format(archive_episode_name)
+    url = f'https://archive.org/details/{archive_episode_name}&output=json'
     return (archive_episode_name, url)
 
 
 def print_summary(stats):
-
     total = sum([v['downloads'] for v in stats.values()])
     average = float(total) / len(stats)
-    most_downloaded = max(*stats.items(), key=lambda x:x[1]['downloads'])
-    least_downloaded = min(*stats.items(), key=lambda x:x[1]['downloads'])
+    most_downloaded = max(*stats.items(), key=lambda x: x[1]['downloads'])
+    least_downloaded = min(*stats.items(), key=lambda x: x[1]['downloads'])
 
-    episodegetter = lambda i: int(i[0].split('-')[-1])
-    for name, data in sorted(stats.items(), key=episodegetter):
-        print('{}: {}'.format(name, data['downloads']))
+    summary = sorted(stats.items(), key=lambda i: int(i[0].split('-')[-1]))
+    for name, data in summary:
+        print(f'{name}: {data["downloads"]}')
 
     print()
-    print('Most downloaded episode: {} ({})'.format(
-        most_downloaded[0], most_downloaded[1]['downloads']))
-    print('Least downloaded episode: {} ({})'.format(
-        least_downloaded[0], least_downloaded[1]['downloads']))
-    print('Total of Downloads: {}'.format(total))
-    print('Average of Downloads: {}'.format(round(average, 2)))
+    print(
+        f'Most downloaded episode: {most_downloaded[0]} '
+        f'({most_downloaded[1]["downloads"]})'
+    )
+    print(
+        f'Least downloaded episode: {least_downloaded[0]} '
+        f'({least_downloaded[1]["downloads"]})'
+    )
+    print(f'Total of Downloads: {total}'.format(total))
+    print(f'Average of Downloads: {round(average, 2)}')
+
 
 async def fetch(session, url):
     """Fetch an URL and returns its JSON response."""
     with async_timeout.timeout(10):
         async with session.get(url) as response:
             return await response.json()
+
 
 async def main(loop, data):
     stats = {}
